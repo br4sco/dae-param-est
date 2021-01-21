cd279783d4cf79e02b8a15fcdf0e73ba522edf73
Mohamed Rasheed Hilmy Mohamed Abdalsalam Abdalmoaty <abda@kth.se>
2021-01-21 09:08:31 +0100

    Add files via upload
    Trying approximate method that maybe used to generate realizations of continuous-time random function. The spectrum is assumed given, and the method relies on a harmonic representation where the spectrum is discretized. The code is not optimized.

---
+++ spectral_Monte_Carlo.m
@@ -0,0 +1,132 @@
+% Given a continuous rational spectrum of a CT stochastic process w(t), 
+% we seek to generate a realization of w
+% It is assumed that we the spectrim phi_w(w) is characterized via a 
+% linear filter Gw with n poles and m zeros such that all poles are stable.
+% The spectrum is discretized and the spectral representation is used.
+
+% === This code is not optimized for computational efficiency === %
+
+% Mohamed Abdalmoaty
+% abda@kth.se
+% January 20, 2021
+
+% ========================================================
+
+close all
+clear
+
+% rng(100) % uncomment to fix realization (done when used for parameter estimation)
+
+%number of required realizations
+M = 1000;
+tfinal = 5; % seconds. This is the length of each realization
+
+
+
+% Suppose that Gw(s) = (s+z1)/(a*s^2 + b*2*s +c)
+% the parameters z1, a, b, and c are known (for now)
+s= tf('s');
+Gw = (s+1)/(s^2 + sqrt(12)*s +2);
+specFac = @(s) (s+1)./(s.^2 + sqrt(12)*s +2);
+
+% bodemag(Gw); % inspect to check frequency w_max, above which the spectrum is
+% almost zero
+
+w_max = 100; % rad/sec
+dw = 0.001; %rad/sec  delta w ( the realizations are periodic with period 2*pi/dw)
+W = dw:dw:w_max;  % a grid of frequencies
+% value of the spectral density at the chosen frequencies
+spec = (specFac(W*1i)).*specFac(-W*1i); 
+% loglog(W,spec)
+k = length(W); % number of used frequencies
+Phi = rand(M,k)*2*pi;
+w = @(t) sum(2*sqrt(dw*spec).*cos(W*t + Phi),2)'; % the process w(t)
+
+wT = zeros(tfinal,M); % allocate memory space
+dt = 0.1; % seconds
+
+% Evaluating w(t) on a uniform time grid for inspection
+
+T = dt:dt:tfinal;
+for i = 1:length(T)
+    wT(i,:) = w(T(i));  
+    % each column of wT is a realization of w(t), 0<t<tfinal
+end
+
+
+% plot the 10th realization
+figure
+plot(T, wT(:,10))
+
+
+figure
+hist(wT(10,:))
+figure
+hist(wT(:,10))
+% statistics
+% figure
+% plot(mean(wT)) % each realization should be zero
+
+% check variances at different times
+% var(wT(100,:))
+% var(wT(500,:))
+% var(wT(end,:))
+var(wT(20,:))
+
+
+% averages
+var(wT(1,:))
+var(wT(:,1))
+
+% variance check
+covar(Gw,1)*(2*pi)
+[A,B,C,D] = tf2ss(Gw.num{1},Gw.den{1});
+C*lyap(A,B*B')*C'*(2*pi)
+
+spect = @(w)   (w.^2 + 1) ./ (w.^4 + 8*w.^2 + 4);
+integral(spect,-inf, inf)
+
+sum(2*dw*spec)
+
+
+% comparizon with exact discrete-time method
+% canonical state-space model corresponding to Gw
+Aw = [0 -2;
+      1 -sqrt(12);];
+Bw = [1; 1;];
+C = [0 sqrt(2*pi)];  % there is a 2*pi factor appearing in C
+                     % I had to add this to match the correlations from the
+                     % two method. I think this has to do with the
+                     % normalization of the correlation function when
+                     % computed using inverse Fourier transforms
+F = [-Aw Bw*Bw';
+    zeros(2) Aw';]*dt;
+expF = expm(F);
+Awd = expF(3:4,3:4)';
+sigma_w = expF(3:4,3:4)'* expF(1:2,3:4);
+
+Gwd = ss(Awd,eye(2),C,0,dt);
+[P,Q] = covar(Gwd,sigma_w) % steady-state covariance of state and output
+for mc = 1:M
+   wTd(:,mc) = lsim(Gwd, chol(sigma_w,'lower')*randn(2,length(T)),T, chol(Q,'lower')*randn(2,1))';
+end
+figure(1)
+hold all
+plot(T,wTd(:,2))
+var(wTd(50,:))
+var(wTd(:,10))
+
+
+
+
+figure
+hist(wTd(10,:))
+figure
+hist(wTd(:,10))
+
+
+figure
+plot(T,wT)
+
+figure
+plot(T,wTd)
