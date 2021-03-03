916f8cd6c31a25fe1d127a36ab2912f5c2d7528a
Oscar Eriksson <oerikss@kth.se>
2021-03-04 00:13:17 +0100

    Homerolled linear interpolation.
    Former-commit-id: 1d14ea7383ed389d1ec566065244c22431553754
    Former-commit-id: 0148a0dbcb789ad5ae598bc3d8bbd315efab2d17

--- src/julia/estimation.jl
+++ src/julia/estimation.jl
@@ -1,5 +1,7 @@
 using Distributed
 using LaTeXStrings
+using Profile
+using Interpolations
 
 include("helpers.jl")
 include("noise_model.jl")
@@ -11,21 +13,36 @@ Random.seed!(seed)
 # === experiment parameters ===
 const Ts = 0.05                                            # stepsize
 
-M = 10                                                     # number of noise realizations
-const m_true = 12                                           # pick the true system
+M = 1000                                                    # number of noise realizations
+const m_true = 12                                          # pick the true system
 const m_u = 1                                              # input realization
 const ms = filter(m -> m != m_true && m != m_u, 1:(M + 2)) # enumerate the realizations
 
 # noise_method_name = "Spectral Monte-Carlo"
 # noise_fun = mk_spectral_mc_noise_model_1(50.0, 0.01, M + 2, 1.0)
 
-const noise_method_name = "Pre-generated unconditioned noise"
-const WS = read_unconditioned_noise_1(1100, 0.005, 100.0)
-noise_fun(m::Int) = interpolation(100.0, WS[:, m])
+const δ = 0.005
+const Tw = 100.0
+const Mw = 1100
+const noise_method_name = "Pre-generated unconditioned noise (δ = $(δ))"
+const WS = read_unconditioned_noise_1(Mw, δ, Tw)
+const K = size(WS, 1) - 1
+const tsw = collect(0:δ:(δ * K))
 
-const u_scale = 0.2                   # input scale
+function noise_fun(m::Int)
+  function w(t::Float64)
+    k = Int(floor(t / δ)) + 1
+    w0 = WS[k, m]
+    w1 = WS[k + 1, m]
+    w0 + (t - (k - 1) * δ) * (w1 - w0) / δ
+  end
+end
+
+# noise_fun(m::Int) = LinearInterpolation(tsw, WS[:, m])
+
+const u_scale = 2.0                   # input scale
 # const w_scale = 0.04                # noise scale
-const w_scale = 0.02                  # noise scale
+const w_scale = 4.0                   # noise scale
 
 u(t::Float64) = u_scale * noise_fun(m_u)(t)
 wm(m::Int) = t -> w_scale * noise_fun(m)(t)
@@ -33,21 +50,21 @@ wm(m::Int) = t -> w_scale * noise_fun(m)(t)
 const σ = 0.002                         # observation noise variance
 
 # === physical model ===
-# const output_state = 1                                       # 1 = x, 3 = y
+const output_state = 1                                       # 1 = x, 3 = y
 # h(sol) = apply_outputfun(x -> x[output_state], sol)          # output function
 h(sol) = apply_outputfun(x -> atan(x[1] / -x[3]), sol)          # output function
 
 const m = 0.3                         # [kg]
 const L = 6.25                        # [m], gives period T = 5s (T ≅ 2√L) not
-                                # accounting for friction.
+                                      # accounting for friction.
 const g = 9.81                        # [m/s^2]
 const k = 0.01                        # [1/s^2]
 
 const θ0 = L                          # We try to estimate the pendulum length
 mk_θs(θ) = [m, θ, g, k]
 
-const φ0 = 0.                         # Initial angle of pendulum from negative
-                                # y-axis
+const φ0 = 0. / 8                     # Initial angle of pendulum from negative
+                                      # y-axis
 
 mk_problem(w, θ, N) = problem(pendulum(φ0, u, w, mk_θs(θ)), N, Ts)
 
@@ -59,22 +76,6 @@ const δθ = 0.05
 
 const θs = (θ0 - Δθ * θ0):δθ:(θ0 + Δθ * θ0) |> collect
 const nθ = length(θs)
-
-# function mk_sim(N)
-#   f(m, θ) = simulate(mk_model(mk_w(m), mk_θs(θ)), N, Ts)
-# end
-#
-# function mk_sim_h(N)
-#   f(m, θ) = simulate_h(mk_model(mk_w(m), mk_θs(θ)), N, Ts, h)
-# end
-#
-# function mk_sim_h_baseline(N)
-#   f(θ) = simulate_h(mk_model(t -> 0., mk_θs(θ)), N, Ts, h)
-# end
-#
-# function mk_sim_h_m(N)
-#   f(ms, θ) = simulate_h_m(m -> mk_model(mk_w(m), mk_θs(θ)), N, Ts, h, ms)
-# end
 
 # Visualize the effect of the noise at the true θ
 function plot_system_at_true_param(N)
@@ -138,42 +139,31 @@ function plot_baseline_costs(N)
   vline!(pl, [θ0], linecolor = :gray, lines = :dot, label="θ0")
 end
 
-# function plot_mean_vs_true_trajectory(N)
-#   sim_h = mk_sim_h(N)
-#   sim_h_m = mk_sim_h_m(N)
-#   sim_h_baseline = mk_sim_h_baseline(N)
-#
-#   σs = σ * rand(Normal(), N + 1)
-#   y = sim_h(m_true, θ0) + σs
-#   yhat = mean(sim_h_m(ms, θ0), dims = 2)
-#   yhat_baseline = sim_h_baseline(θ0)
-#
-#   pl = plot(xlabel="time [s]",
-#             title = "u_scale = $(u_scale), w_scale = $(w_scale), N = $(N)")
-#
-#   plot!(pl, [yhat y], fillrange=[y yhat], fillalpha=0.2, c=:yellow, label="")
-#   plot!(pl, [yhat_baseline y], fillrange=[y yhat_baseline], fillalpha=0.2, c=:blue, label="")
-#   plot!(pl, yhat, linecolor = :green, linewidth = 1, label = "mean")
-#   plot!(pl, yhat_baseline, linecolor = :blue, linewidth = 1, label = "baseline")
-#   plot!(pl, y, linecolor = :red, linewidth = 1, label = "true trajectory")
-# end
-#
-# function plot_baseline_vs_true_trajectory(N)
-#   sim_h_baseline = mk_sim_h_baseline(N)
-#   sim_h = mk_sim_h(N)
-#
-#   σs = σ * rand(Normal(), N + 1)
-#   y = sim_h(m_true, θ0) + σs
-#   yhat = sim_h_baseline(θ0)
-#
-#   pl = plot(xlabel="time [s]",
-#             title = "u_scale = $(u_scale), w_scale = $(w_scale), N = $(N)")
-#
-#   plot!(pl, [yhat y], fillrange=[y yhat], fillalpha=0.2, c=:yellow, label="")
-#   plot!(pl, yhat, linecolor = :green, linewidth = 1, label = "baseline")
-#   plot!(pl, y, linecolor = :red, linewidth = 1, label = "true trajectory")
-# end
-#
+function plot_mean_vs_true_trajectory(N)
+  T = N*Ts
+  ts = 0:Ts:T
+
+  solvew(w, θ) = solve(mk_problem(w, θ, N), saveat=ts) |> h
+
+  σs = σ * rand(Normal(), N + 1)
+  y = solvew(wm(m_true), θ0) + σs
+  yhat = mean(solve_m(m -> solvew(wm(m), θ0), N, ms), dims = 2)
+  yhat_baseline = solvew(t -> 0., θ0)
+
+  pl = plot(xlabel="time [s]",
+            title = "u_scale = $(u_scale), w_scale = $(w_scale), N = $(N)")
+
+  plot!(pl, [yhat y], fillrange=[y yhat],
+    fillalpha=0.2, c=:yellow, label="")
+
+  plot!(pl, [yhat_baseline y], fillrange=[y yhat_baseline],
+    fillalpha=0.2, c=:blue, label="")
+
+  plot!(pl, y, linecolor = :red, linewidth = 1, label = "true trajectory")
+  plot!(pl, yhat, linecolor = :green, linewidth = 1, label = "mean")
+  plot!(pl, yhat_baseline, linecolor = :blue, linewidth = 1, label = "baseline")
+end
+
 function run(id, N)
   T = N*Ts
   ts = 0:Ts:T
@@ -212,8 +202,7 @@ function run(id, N)
     w_scale = w_scale,
     noise_method_name = noise_method_name,
     seed = seed,
-    m_true = m_true,
-    output_state = output_state
+    m_true = m_true
   )
 
   CSV.write(joinpath("data", "$(filename)_data.csv"), data)
