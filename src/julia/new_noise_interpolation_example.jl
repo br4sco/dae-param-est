0741d3c60291761e81afbdfc3d5e315c25fc913f
Robert Bereza <robbj@kth.se>
2021-03-09 10:41:44 +0100

    Removed old example file
    Replaced contents of new_noise_interpolation_example with content from newer_noise_interpolation_example and deleted the latter file
    
    
    Former-commit-id: 347716135f04b34ea9d6429a6357acad493f2b44
    Former-commit-id: f9e5fdae07ef64376bcc5909af5e4eb9fd58c314

--- src/julia/new_noise_interpolation_example.jl
+++ src/julia/new_noise_interpolation_example.jl
@@ -24,26 +24,39 @@ C = [0 1]
 # C = [0 0 0 1]
 
 const Ts = 0.05             # Sampling frequency of noise model
-save_data = false
-N = 100     # Noise samples, excluding the initial one, x_e(0)
+# N = 100     # Noise samples, excluding the initial one, x_e(0)
+N = 2     # Noise samples, excluding the initial one, x_e(0)
 M = 100
 P = 4       # Number of inter-sample samples stored
+Q = 4       # Number of inter-sample states stored
+# N = 8     # Noise samples, excluding the initial one, x_e(0)
+# M = 2
+# P = 1       # Number of inter-sample samples stored
 nx = size(A)[1]
 noise_model = discretize_ct_noise_model(A, B, C, Ts, zeros(nx,))
 noise_uniform_dat, noise_inter_dat = generate_noise_new(N, M, P, nx)
 # Computes all M realizations of filtered white noise
 x_mat = simulate_noise_process_new(noise_model, noise_uniform_dat)
 
-num_samples = zeros(Int64, N)
+isd = initialize_isd(Q, N, nx)
 
-# Using only the first realization right now. Each column of x_mat corresponds to
+m = 1
+
+# Using only the m:th realization right now. Each column of x_mat corresponds to
 # one realization, and each row to one time instant. Each element is itself
 # a state-vector (Array{Float64, 1}), not a scalar
 function w(t::Float64)
-    return (C*noise_inter(t, Ts, A, B, x_mat[:, 1], noise_inter_dat[1], num_samples, noise_model.x0))[1]
+    return (C*noise_inter(t, Ts, A, B, x_mat[:, m], noise_inter_dat[m],
+            isd, noise_model.x0))[1]
 end
 
 δ = 0.025
 
-plot(0:0.01:N*Ts, w)
-# savefig("./plots.png")
+# plot(0:0.001:N*Ts, w)
+
+# t = 0.0172
+t = 0.0368
+# t = 0.172
+# t = 0.242
+plot(0:0.001:N*Ts, w)
+# # # savefig("./plots.png")
