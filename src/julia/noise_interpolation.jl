542ca1f2d42ecd162a2fe0cb2c54d0e9129f13f7
Robert Bereza <robbj@kth.se>
2021-05-01 15:56:44 +0200

    Added functionality for more memory-efficient storage of inter sample data
    Added functionality for more memory-efficient storage of inter sample data in the form of a struct called InterSampleWindow and associated functions
    
    
    Former-commit-id: f51df2cd519c8fab0c100287ebe68d5585c6a228 [formerly 5887f875ce65d59ea4d74d623ed0773059dcfa90]
    Former-commit-id: 42ac8f7b46be0b92b1cc52b82f9e380c54d5fe70

--- src/julia/noise_interpolation.jl
+++ src/julia/noise_interpolation.jl
@@ -1,5 +1,18 @@
 using Random, LinearAlgebra, Future
 
+mutable struct InterSampleWindow
+    containers::Array{Array{Float64,2},1}
+    sample_times::Array{Array{Float64,1},1}
+    Q::Int64    # Max number of stored samples per interval
+    W::Int64    # Number of containers in the array containers
+    # TODO: Not sure if this struct is the best place to store use_interpolation
+    use_interpolation::Bool # true if linear interpolation of states is used
+    # instead of conditional sampling when Q stored samples has been surpassed.
+    # It improves smoothness of realization in such a scenario.
+    start::Int64    # The interval index to which the "first" container corresponds
+    ptr::Int64      # The index of the "first" container in the array containers
+end
+
 mutable struct InterSampleData
     # states[i][p,j] is the j:th element of the p:th state in the i:th interval
     # The number of rows of states[i] should be dynamically updated as new
@@ -12,6 +25,88 @@ mutable struct InterSampleData
     use_interpolation::Bool # true if linear interpolation of states is used
     # instead of conditional sampling when Q stored samples has been surpassed.
     # It improves smoothness of realization in such a scenario.
+end
+
+function initialize_isw(Q::Int64, W::Int64, nx::Int64,
+     use_interpolation::Bool=true)::InterSampleWindow
+     if Q > 0
+         containers = [zeros(0,nx)  for j=1:W]
+         sample_times = [zeros(0) for j=1:W]
+     else
+         isd_states = [zeros(0,nx)]
+         isd_sample_times = [zeros(0)]
+     end
+     return InterSampleWindow(containers, sample_times, Q, W, use_interpolation, 0, 1)
+end
+
+function map_to_container(num::Int64, isw::InterSampleWindow)
+    return ((num-1)%isw.W) + 1
+end
+
+function add_sample(x_new::Array{Float64, 1}, sample_time::Float64, n::Int64,
+    isw::InterSampleWindow)
+
+    if isw.start <= n && isw.start >= isw.start + isw.W - 1
+        container_id = map_to_container(isw.ptr + n - start, isw)
+    elseif n > isw.start + isw.W - 1
+        num_steps = n - isw.start - isw.W + 1
+        for step in 1:num_steps
+            isw.containers[map_to_container(isw.ptr+isw.W-1+step, isw)] = zeros(0, size(x_new)[1])
+            isw.sample_times[map_to_container(isw.ptr+isw.W-1+step, isw)] = zeros(0)
+        end
+        container_id = map_to_container(isw.ptr+isw.W-1+num_steps)
+        isw.ptr = map_to_container(isw.ptr+num_steps, isw)
+        isw.start += num_steps
+    else # n < isw.start
+        @warn "Tried to add sample outside of inter-sample window"
+        return
+    end
+    # Only stores samples if less than Q samples are already stored
+    if size(isw.containers[container_id])[1] < isw.Q
+        push!(isw.containers[container_id], x_new')
+        push!(isw.sample_times[container_id], sample_time)
+    end
+end
+
+function get_neighbors(n::Int64, t::Float64, x::Array{Array{Float64, 1}, 1},
+    Ts::Float64, isw::InterSampleWindow)
+    tl = n*Ts
+    tu = (n+1)*Ts
+    il = 0
+    iu = 0
+    if n >= isw.start && n <= isw.start + isw.W - 1
+        idx = map_to_container(isw.ptr + n - isw.start, isw)
+        num_stored_samples = size(isw.containers[idx])[1]
+        if num_stored_samples > 0
+            # TODO: There must be a more efficient search method
+            for q=1:num_stored_samples
+                t_inter = isw.sample_times[idx][q]
+                if t_inter > tl && t_inter <= t
+                    tl = t_inter
+                    il = q
+                end
+                if t_inter < tu && t_inter >= t
+                    tu = t_inter
+                    iu = q
+                end
+            end
+        end
+    end
+
+    if il > 0
+        xl = view(isw.containers[idx], il, :)
+    else
+        xl = view(x, n+1)
+    end
+    if iu > 0
+        xu = view(isw.containers[idx], iu, :)
+    else
+        xu = view(x, n+2)
+    end
+    δl = t-tl
+    δu = tu-t
+    return xu, xl, δu, δl
+
 end
 
 function initialize_isd(Q::Int64, N::Int64, nx::Int64, use_interpolation::Bool)::InterSampleData
@@ -29,7 +124,7 @@ function noise_inter(t::Float64,
                      Ts::Float64,       # Sampling time of noise process
                      A::Array{Float64, 2},
                      B::Array{Float64, 2},
-                     x::Array{Array{Float64, 1}},
+                     x::Array{Array{Float64, 1}, 1},
                      isd::InterSampleData,
                      ϵ::Float64=10e-12,
                      rng::MersenneTwister=Random.default_rng())
@@ -107,7 +202,7 @@ function noise_inter(t::Float64,
     # linear interpolation instead, to ensure smoothness of realization
     if num_stored_samples >= Q && use_interpolation
         # @warn "Used linear interpolation"   # DEBUG
-        return xl + (xu-xl)*(t-tl)/(tu-tl)
+        return xl + (xu-xl)*δl/δu
     end
 
     # Values of δ smaller than ϵ are treated as 0
