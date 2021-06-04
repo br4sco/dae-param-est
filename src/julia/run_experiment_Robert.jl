623fb17ad6fe8d84ee4574f4a84a77a4ef255bc6
Robert Bereza <robbj@kth.se>
2021-06-04 10:10:13 +0200

    Fixed typo in previous commit
    Former-commit-id: d17eea7aedb63363f28ba188c503918edbdcabf6 [formerly 0339c82adffa9dc147b94cf01ea235fd396e5e4f]
    Former-commit-id: 21725f8f6ce6c43ef0b9d517102548230377ebaa

--- src/julia/run_experiment_Robert.jl
+++ src/julia/run_experiment_Robert.jl
@@ -2,7 +2,7 @@ include("data-gen.jl")
 
 Ym = calc_mean_Y()
 try
-    write_mean_Y("idtsheta100000", Ym)
+    write_mean_Y("idtheta100000", Ym)
 catch e
     @warn "Error while storing Ym, make sure to do it manually"
     println(e)
