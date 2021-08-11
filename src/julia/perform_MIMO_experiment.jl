8dc745185eea910b65d00e57da4b603e7957de7f
Robert Bereza <robbj@kth.se>
2021-08-11 15:39:24 +0200

    Added storage of Y and metadata to MIMO experiment
    Added storage of Y and metadata to MIMO experiment, since generating Y using exact interpolation anew every time takes a very long time
    
    
    Former-commit-id: 2d25c9f202d919fd8c7cc4a428f665b054095409 [formerly ccec1100be9a8aabe196a5ccb4a1b46a6d8e29ae]
    Former-commit-id: 6f5f83a386a5d74e00fd4c6acad46666d40e75e9

--- src/julia/perform_MIMO_experiment.jl
+++ src/julia/perform_MIMO_experiment.jl
@@ -1,11 +1,35 @@
+using Dates
 include("MIMO_experiment.jl")
+
+function write_Y_and_metadata(Y)
+    datetime_str = replace(string(now()), ":" => "_")
+    metadata = "θ0: $(string(mk_θs(θ0)))
+η0: $(string(η0))
+nx: $nx
+n_out: $n_out
+n_in: $n_in
+n_u: $n_u
+T: $T
+δ: $(δ_min)
+Ts: $Ts
+W: $W
+Q: $Q"
+    p_Y = joinpath(data_dir, ("Y"*datetime_str)*".csv")
+    p_metadata = joinpath(data_dir, ("Y_metadata"*datetime_str)*".txt")
+    writedlm(p_Y, Y, ",")
+    file_meta = open(p_metadata, "w")
+    write(file_meta, metadata)
+    close(file_meta)
+end
 
 try
     Y = calc_Y()
+    write_Y_and_metadata(Y)
     perform_experiments(Y, vcat(θ0, w_scale))
 catch e
+    print(e)
     p = joinpath(data_dir, "error_msg.txt")
     file = open(p, "w")
-    write(file, sprint(showerror, backtrace()))
+    write(file, sprint(showerror, e, catch_backtrace()))
     close(file)
 end
