61ef5defbf7094a6b65206ab1ae3dcd374cd70c7
Robert Bereza <robbj@kth.se>
2022-04-06 15:21:40 +0200

    Major clean-up of code and deletion of old files
    Deleted all files not used for the experiments for CDC2022, and cleaned the remaining files up a bit, e.g. by renaming them or removing code for old SGD not using ADAM.

--- src/julia/run_experiment_Robert.jl
+++
@@ -1,9 +0,0 @@
-include("data-gen.jl")
-
-Ym = calc_mean_Y()
-try
-    write_mean_Y("paper_experiment_50000", Ym)
-catch e
-    @warn "Error while storing Ym, make sure to do it manually"
-    println(e)
-end
