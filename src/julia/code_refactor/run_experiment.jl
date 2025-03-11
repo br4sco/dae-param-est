0aa41def50b4fcc721695940b18126b3884c989c
Robert Marczuk Bereza-Jarocinski <robert.bereza@proton.me>
2025-03-11 15:45:43 +0100

    Adjusted disturbance learning rate to work with new disturbance model
    The new disturbance model refers to when poles have gotten their distance to the origin increased by a factor 500. This affects the parameter's order of magnitude and warrants different initial learning rates

--- src/julia/code_refactor/run_experiment.jl
+++ src/julia/code_refactor/run_experiment.jl
@@ -65,7 +65,7 @@ const Q::Int = 1000          # Number of conditional samples stored per interval
 # The components corresponding to the free disturbance parameters η are picked out later
 # using a call to get_disturbance_free_pars
 # dist_init_learning_rate = [0.1, 0.2, 0.2, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05] # Small disturbance model
-dist_init_learning_rate = vcat([0.1, 0.2, 0.2, 0.05, 0.05, 0.05], zeros(9), [0.05, 0.05, 0.05], zeros(9), [0.05, 0.05, 0.05]) # Large disturbance model
+dist_init_learning_rate = vcat([500*0.1, (500^2)*0.2, (500^3)*0.2, 0.05, 0.05, 0.05], zeros(9), [0.05, 0.05, 0.05], zeros(9), [0.05, 0.05, 0.05]) # Large disturbance model
 # Similarly for disturbance parameter bounds
 dist_bounds = repeat([-Inf Inf], 30)
 
